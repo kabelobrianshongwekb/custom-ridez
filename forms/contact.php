5d6486a915d15af855182f1c3aaee40cb4e239b1
kabelo <shongwekabelo0@gmail.com>
2023-04-13 09:29:59 +0200

    changed logo  size

--- forms/contact.php
+++ forms/contact.php
@@ -9,18 +9,18 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'shongwekabelo0@gmail.com';
 
+
   
-  $contact = new mail;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$receiving_email_address;
+  $from_name = $_POST['name'];
+  $from_email = $_POST['email'];
+  $subject = $_POST['subject'];
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  $contact = new mail($receiving_email_address, );
   echo $contact->send();
 ?>
